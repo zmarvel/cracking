e4e1373fdaf8a2dfda5be314c626626e87506b42
Jessie Pusateri <jessiepusateri@gmail.com>
2016-09-09 13:10:28 -0400

    Update 1_sorted_merge.py

--- ch10/1_sorted_merge.py
+++ ch10/1_sorted_merge.py
@@ -5,21 +5,19 @@ A = [1, 3, 4, 6, None,  None]
 B = [2, 5]
 
 def merge_two_sorted(list1, list2):
-    list1_len = len(list1)
-    index1 = list1_len - len(list2) - 1 #end of list1
+    index1 = len(list1) - len(list2) - 1 #end of list1
     index2 = len(list2) - 1 #end of list2
-    index3 = list1_len #beginning of placement
-    for i in range(list1_len, -1, -1):
-        if list1[index1] > list2[index2]:
-            tmp = list1[index3]
-            list1[index3] = list1[index2]
-            list1[index2] = tmp
+    index3 = len(list1) - 1 #beginning of placement
+    for i in range(index3, -1, -1):
+        if index2 < 0 or list1[index1] > list2[index2]:
+            list1[i] = list1[index1]
+            index1 -= 1
         else:
-            tmp = list1[index3]
-            list1[index3] = list1[index2]
-            list1[index2] = tmp
+            list1[i] = list2[index2]
+            index2 -= 1
+        index3 -= 1
     return list1
-
+    
 print merge_two_sorted(A,B)
 
 
