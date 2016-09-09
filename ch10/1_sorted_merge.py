168e744a4d81934a1c91525b4e15e4a1c5fd607f
Zack Marvel <zpmarvel@gmail.com>
2016-09-08 21:03:58 -0400

    Added my solution to sorted merge.

--- ch10/1_sorted_merge.py
+++ ch10/1_sorted_merge.py
@@ -1,3 +1,6 @@
+
+# Jessie
+
 A = [1, 3, 4, 6, None,  None]
 B = [2, 5]
 
@@ -18,3 +21,28 @@ def merge_two_sorted(list1, list2):
     return list1
 
 print merge_two_sorted(A,B)
+
+
+
+# Zack, python3
+
+def sorted_merge(A, B, n, m):
+    assert len(B) == m
+    assert (m + n) == len(A)
+
+    idxA = n-1
+    idxB = m-1
+    head = len(A)-1
+    while head >= 0:
+        if idxB < 0 or A[idxA] > B[idxB]:
+            A[head] = A[idxA]
+            idxA -= 1
+        else:
+            A[head] = B[idxB]
+            idxB -= 1
+        head -= 1
+    return A
+
+assert sorted_merge([1, 2, 3, 4, 5, None, None], [6, 7], 5, 2) == [1, 2, 3, 4, 5, 6, 7]
+assert sorted_merge([1, 3, 5, None, None], [2, 4], 3, 2) == [1, 2, 3, 4, 5]
+assert sorted_merge([-2, 0, 0, 3, None, None, None], [-1, 2, 4], 4, 3) == [-2, -1, 0, 0, 2, 3, 4]
